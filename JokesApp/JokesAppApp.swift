f6d13d1be597eedbbc163ef923b07a5efc8539b0
Yusufihsangorgel <87409084+Yusufihsangorgel@users.noreply.github.com>
2022-12-13 18:44:18 +0300

    Initial Commit

---
+++ JokesApp/JokesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppApp.swift
+//  JokesApp
+//
+//  Created by Yusuf İhsan Görgel on 13.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
